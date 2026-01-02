dce5a94d431d6409eee2f1764035842b054d3bf0
hazzaz-am <hazzazabdulmannan@gmail.com>
2026-01-02 23:01:19 +0600

    implement authentication using better-auth library

---
+++ project-one/lib/auth.ts
@@ -0,0 +1,15 @@
+import { betterAuth } from "better-auth";
+
+export const auth = betterAuth({
+	baseURL: process.env.BETTER_AUTH_URL,
+	socialProviders: {
+		github: {
+			clientId: process.env.GITHUB_CLIENT_ID as string,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+		},
+		google: {
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+		},
+	},
+});
